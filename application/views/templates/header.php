397a85d5ae55aba6119f126a38b30f37edb7a1fa
Dragoshi <simon@dragoshi.co.uk>
2017-06-15 08:49:12 +0100

    Added initial views

---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $title; ?></title>
+
+        <!-- Local Bootstrap -->
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+
+        <!-- Local Bootstrap - Optional theme -->
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.min.css">
+
+        <!-- Local jQuery (necessary for Bootstraps JavaScript plugins) -->
+        <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+
+        <!-- Local Bootstrap - Latest compiled and minified JavaScript -->
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+
+        <!-- Local Bootstrap - Latest compiled and minified JavaScript -->
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+
+        <!-- Site Stylesheet -->
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css">
+    </head>
+    <body>
